dc1ed7b6956ce334d8fae9b5f01dfde2a59864a2
Vinicius Baumann Ladosky <vinicius.ladosky@gmail.com>
2024-04-02 18:10:17 -0300

    entrar(), getUltimasLeituras(), getEstacoes()  e addEstacao()

--- js/phpApi.js
+++ js/phpApi.js
@@ -1,29 +1,52 @@
-
-function entrar(criar = false) {
-  let tfUsername = document.getElementById("username");
-  let pfPassword = document.getElementById("password");
-
-  fetch(`php/webApi/${criar ? "signup.php" : "login.php"}`, {
+//Retorna uma lista de erros. Caso vazia, o login foi um sucesso.
+//{erro: string}[]
+function entrar(username, password, criar = false) {
+  return fetch(`php/webApi/${criar ? "signup.php" : "login.php"}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: `username=${tfUsername.value}&password=${pfPassword.value}`,
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      if (json.erros.length == 0) {
-        window.location.replace("painel.html");
-      } else {
-        //limpar senha
-        pfPassword.value = "";
-
-        //listar erros
-        document.getElementById("erros").innerHTML = json.erros
-          .map((erro) => `<li>${erro}</li>`)
-          .join("");
-      }
-    });
+    body: `username=${encodeURIComponent(
+      username
+    )}&password=${encodeURIComponent(password)}`,
+  }).then((response) => response.json());
 }
 
-function get_user_id() {}
+/*
+  {
+    id: number, 
+    id_estacao: string, 
+    umidade_ar: number, 
+    temperatura: number, 
+    umidade_solo: number, 
+    pluv_indice: number, 
+    horario: string(H:i:s d-m-y)
+  }[]
+*/
+function getUltimasLeituras(id_estacao, limite = 1) {
+  return fetch(
+    `php/webApi/get_ultimas_leituras.php?limite=${encodeURIComponent(
+      limite.toString()
+    )}&id_estacao=${encodeURIComponent(id_estacao)}`
+  ).then((response) => response.json());
+}
+
+//estacoes: {id: string, nome: string, localizacao: string}[]
+function getEstacoes(limite = 10) {
+  return fetch(
+    `php/webApi/get_estacoes.php?limite=${encodeURIComponent(
+      limite.toString()
+    )}`
+  ).then((response) => response.json());
+}
+
+//retorna a estação adicionada
+//{erros: string[] id: string, nome: string, localizacao: string}
+function addEstacao(id_estacao, nome) {
+  return fetch(
+    `php/webApi/add_estacao.php/?id_estacao=${encodeURIComponent(
+      id_estacao
+    )}&nome=${encodeURIComponent(nome)}`
+  ).then((response) => response.json());
+}
+
